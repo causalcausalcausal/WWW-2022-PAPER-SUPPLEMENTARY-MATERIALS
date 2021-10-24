d6d2db1f0401642a1afe166d5c45baaa0e734729
Atlas Yu <Atlasyu@users.noreply.github.com>
2021-10-24 16:06:30 +0800

    Add files via upload

---
+++ Code/Model/LBCF/LBCF_budget_allocation-RCT.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from DGB_RCT import DGB
+
+test = pd.read_csv("../../../Data/RCT_data/rct_test.csv" )
+test_result = pd.read_csv("output/UDCF_uplift_RCT",header=None,names=['pred1','pred2','pred3','pred4','pred5','pred6','pred7'])
+
+test[['pred1','pred2','pred3','pred4','pred5','pred6','pred7']]=test_result
+test_pivot = test.copy()
+cost = np.array([0.3, 0.5, 0.7, 1.0, 1.3, 1.6, 1.9])
+value = test_pivot[['pred1','pred2','pred3','pred4','pred5','pred6','pred7']].to_numpy()
+
+for budget in [300,400,500,600,700]:
+    model = DGB(budget,cost,value)
+    model.train()
+    df_dgb = model.save_to_dataframe(test_pivot)
+    indices, values, spend = model.generate_decisions()
+    df_dgb['final_coin'] = df_dgb['cost']
+    coin_map = {0:0.1,1: 0.4,2:0.6,3:0.8,4:1.1,5:1.4,6:1.7,7:2.0}
+    test_pivot['treatment'] = test_pivot['exp_group'].map(coin_map)
+    test_pivot['final_coin'] = test_pivot['final_coin'].round(1)
+    test_pivot['treatment'] = test_pivot['treatment'].round(1)
+    test_pivot['if_same'] = test_pivot['treatment'] == test_pivot['final_coin']
+    test_pivot['tmp_label'] =  np.where(test_pivot['if_same'],test_pivot['treatment'],0)
+    test_pivot.to_csv(str(budget)+'RCT_LBCF_final.csv')
